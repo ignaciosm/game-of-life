56d388889c94642df5fe501292640ccf5bf177f5
Ignacio San Martin <ignaciosm@gmail.com>
2020-06-20 19:17:55 -0500

    progress in generating loops

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback, useRef, useEffect } from 'react'
 
 const Grid = () => {
 
@@ -8,39 +8,54 @@ const Grid = () => {
     const buildGrid = () => {
         let cols = [];
         for (let i = 0; i < numCols; i++) {
-            cols.push(Array.from(Array(numRows), () => 0))
+            cols.push(Array.from(Array(numRows), () => Math.floor(Math.random() * 2)))
         }
         return cols
     }
 
+    // STATE
     const [grid, setGrid] = useState(buildGrid())
+    // const [gridNew, setGridNew] = useState(buildGrid())
+    const [running, setRunning] = useState(false)
+
+    // const toggle = () => {
+    //     setRunning(!running)
+    // }
+
+    // useEffect(() => {
+    //     running ?
+    //         setGrid(buildGrid()) :
+    //         console.log('stop')
+    // })
 
     console.log(grid)
 
     return (
+        <>
+            {/* <button onClick={toggle}>{running ? 'stop' : 'start'}</button> */}
+            <div
+                style={{
+                    display: 'grid',
+                    gridTemplateColumns: `repeat(${numCols}, 20px)`
+                }}
+            >
+                {
+                    grid.map((col, x) => (
+                        col.map((row, y) => (
+                            <div key={`${x}-${y}`}
+                                style={{
+                                    width: 20,
+                                    height: 20,
+                                    background: grid[x][y] ? 'black' : 'white',
+                                    border: 'solid 1px black'
+                                }}>
 
-        <div
-            style={{
-                display: 'grid',
-                gridTemplateColumns: `repeat(${numCols}, 20px)`
-            }}
-        >
-            {
-                grid.map((col, x) => (
-                    col.map((row, y) => (
-                        <div key={`${x}-${y}`}
-                            style={{
-                                width: 20,
-                                height: 20,
-                                background: grid[x][y] ? 'black' : 'white',
-                                border: 'solid 1px black'
-                            }}>
-
-                        </div>
+                            </div>
+                        ))
                     ))
-                ))
-            }
-        </div >
+                }
+            </div >
+        </>
     )
 }
 
